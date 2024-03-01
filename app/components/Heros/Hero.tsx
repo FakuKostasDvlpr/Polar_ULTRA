e2eff1430134636d4d0d45efaa4e057ab8822f2a
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-01 19:53:57 -0300

    mensaje

--- app/components/Heros/Hero.tsx
+++ app/components/Heros/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
         fill="white"
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-yellow-400 bg-opacity-50">
+        <h1 className="text-6xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-yellow-400 bg-opacity-50">
           Nuestros <br /> Servicios
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
--- app/components/cards/services-card-.tsx
+++ app/components/cards/services-card-.tsx
@@ -9,48 +9,49 @@ export default function App() {
   return (
     <>
     {/* 1 */}
-    <div className="flex gap-10 m-10">
-      <div className="grow h-[200px]">
-          <div className="card h-[200px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
-            <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
-              <Security/>
-              <h2 className="card-title font-medium">Seguridad</h2>
-              <p className="hidden sm:block">Ofrecemos films antivandalicos que fortalecen la seguridad de tu espacio, proporcionando una capa adicional de protección contra posibles daños.</p>
-            </div>
+    <div className="flex flex-col">
+      <div className="flex gap-10 m-10">
+        <div className="grow h-[220px]">
+            <div className="card h-[220px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
+              <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
+                <Security/>
+                <h2 className="card-title font-medium">Seguridad</h2>
+                <p className="hidden sm:block">Ofrecemos films antivandalicos que fortalecen la seguridad de tu espacio, proporcionando una capa adicional de protección contra posibles daños.</p>
+              </div>
+          </div>
+        </div>
+        <div className="grow gap-10 h-[220px]">
+            <div className="card  h-[220px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
+              <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
+                <Privacidad/>
+                <h2 className="card-title font-medium">Privacidad</h2>
+                <p className="hidden sm:block">Transformamos tus espacios con materiales espejados y esmerilados, brindando la privacidad necesaria sin comprometer el estilo y la estética de tu entorno.</p>
+              </div>
+          </div>
         </div>
       </div>
-      <div className="grow gap-10 h-[200px]">
-          <div className="card  h-[200px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
-            <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
-              <Privacidad/>
-              <h2 className="card-title font-medium">Privacidad</h2>
-              <p className="hidden sm:block">Transformamos tus espacios con materiales espejados y esmerilados, brindando la privacidad necesaria sin comprometer el estilo y la estética de tu entorno.</p>
+      {/* 2 */}
+      <div className="flex gap-10 m-10">
+        <div className="grow h-[220px]">
+            <div className="card h-[220px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
+              <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
+                <Temp/>
+                <h2 className="card-title font-medium">Aislamiento termico</h2>
+                <p className="hidden sm:block">Adaptamos nuestras soluciones a tus necesidades específicas, ofreciendo una amplia gama de films especiales que cumplen con los requisitos técnicos y estéticos de cada proyecto.</p>
+              </div>
+          </div>
+        </div>
+          <div className="grow h-[220px]">
+              <div className="card h-[220px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
+                <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
+                  <Money/>
+                  <h2 className="card-title font-medium">Aislamiento termico</h2>
+                  <p className="hidden sm:block">Adaptamos nuestras soluciones a tus necesidades específicas, ofreciendo una amplia gama de films especiales que cumplen con los requisitos técnicos y estéticos de cada proyecto.</p>
+                </div>
             </div>
+          </div>
         </div>
       </div>
-    </div>
-    {/* 2 */}
-    <div className="flex gap-10 m-10">
-
-      <div className="grow h-[200px]">
-          <div className="card h-[200px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
-            <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
-              <Temp/>
-              <h2 className="card-title font-medium">Aislamiento termico</h2>
-              <p className="hidden sm:block">Adaptamos nuestras soluciones a tus necesidades específicas, ofreciendo una amplia gama de films especiales que cumplen con los requisitos técnicos y estéticos de cada proyecto.</p>
-            </div>
-        </div>
-      </div>
-      <div className="grow h-[200px]">
-          <div className="card h-[200px] bg-white flex items-center card max-w-md mx-auto justify-center bg-neutral text-gray-300">
-            <div className="mb-3 gap-3 card-body text-center items-center flex justify-center flex-col">
-              <Money/>
-              <h2 className="card-title font-medium">Aislamiento termico</h2>
-              <p className="hidden sm:block">Adaptamos nuestras soluciones a tus necesidades específicas, ofreciendo una amplia gama de films especiales que cumplen con los requisitos técnicos y estéticos de cada proyecto.</p>
-            </div>
-        </div>
-      </div>
-    </div>
     </>
   );
 }
