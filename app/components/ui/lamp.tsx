a2d19d93345b6eb64d476912ec2340ea21a9553f
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-03 12:36:07 -0300

    last

--- app/components/ui/lamp.tsx
+++ app/components/ui/lamp.tsx
@@ -9,7 +9,7 @@ export function LampDemo() {
     <LampContainer>
       <motion.h1
         initial={{ opacity: 0.5, y: 220 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: 1, y: 23 }}
         transition={{
           delay: 0.3,
           duration: 0.9,
