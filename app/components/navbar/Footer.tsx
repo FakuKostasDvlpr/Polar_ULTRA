60d6c96ccae0b94f9ab8bfd8d465563d9149fecf
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-03 18:16:09 -0300

    last

--- app/components/navbar/Footer.tsx
+++ app/components/navbar/Footer.tsx
@@ -1,8 +1,16 @@
+import "../../globals.css"
 export default function Footer() {
     return (
-      <main className="">
-        <h1>FOOTER</h1>
-      </main>
+      <>
+        <footer className="card ">
+          <div className="w-full mx-auto max-w-screen-xl p-4 md:items-center md:justify-between">
+              <span className="text-sm text-b sm:text-center dark:text-black-300 font-bold">
+                <a href="https://www.instagram.com/polarsystem._/" target="_blank" className="hover:underline cursor-pointer">Obras |PolarSystem ©2021</a>
+              </span>
+          </div>
+      </footer>
+
+      </>
     );
   }
   
