35f944948f874e97158c5bed01b7ce5a7b4b6da1
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-09 23:16:40 -0300

    Chip3

--- app/components/ui/Chips.tsx
+++ app/components/ui/Chips.tsx
@@ -7,7 +7,7 @@ export default function Chips() {
   const ChipsItems = ["Ploteo para tu auto", "Pulido de opticas", "Instalacion de alarmas", "Reparacion de aire"];
 
   return (
-      <RevealWrapper delay={700} distance="50px" duration={1200} easing='cubic-bezier(0.5, 0, 0, 1)' origin="right" reset={true} >
+  <RevealWrapper delay={700} distance="50px" duration={1200} easing='cubic-bezier(0.5, 0, 0, 1)' origin="right" reset={true} >
     <div className="grid grid-cols-2 m-2 gap-4 items-center justify-center">
       {ChipsItems.map((chip, index) => (
         <Chip
@@ -21,7 +21,7 @@ export default function Chips() {
         </Chip>
       ))}
     </div>
-      </RevealWrapper >
+  </RevealWrapper >
 
   );
 }
