92c2c69d6b2dff7d16b2bda396608015e39f5924
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-09 23:31:54 -0300

    Stroke Width remove and mB in first div

--- app/components/cards/Obras-colocacion.tsx
+++ app/components/cards/Obras-colocacion.tsx
@@ -2,13 +2,13 @@ import "../../globals.css"
 export default function ObrasTipo() {
     return (
         <>
-           <div className="headline mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex-row flex gap-[3rem] mt-10 flex-wrap justify-center items-center ">
+           <div className="headline mb-14 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex-row flex gap-[3rem] mt-10 flex-wrap justify-center items-center ">
             <div
                 className="rounded-xl flex flex-col cursor-pointer group relative overflow-hidden card px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
                 <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
                 <div className="relative z-10 mx-auto max-w-md">
                     <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-building-store" width="34" height="34" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" /><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-building-store" width="34" height="34" viewBox="0 0 24 24" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" /><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg>
                     </span>
                     <p className="items-center text-center font-bold text-white mt-10">Locales</p>
                     <div
