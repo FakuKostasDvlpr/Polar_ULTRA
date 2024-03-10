8d8377f830fbc3e0f589008727c83ffce1550da8
FakuKostasDvlpr <rollinsh91@gmail.com>
2024-03-09 23:12:34 -0300

    Chips

--- app/components/ui/Chips.tsx
+++ app/components/ui/Chips.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Chip } from "@nextui-org/react";
 import { CheckIcon } from "./CheckIcon";
+import { RevealWrapper } from "next-reveal";
 
 export default function Chips() {
   const ChipsItems = ["Ploteo para tu auto", "Pulido de opticas", "Instalacion de alarmas", "Reparacion de aire"];
 
   return (
-    <div className="flex flex-col mt-5 gap-4">
+    <div className="grid grid-cols-2 m-2 gap-4 items-center justify-center">
       {ChipsItems.map((chip, index) => (
+      <RevealWrapper delay={700} distance="50px" duration={1200} easing='cubic-bezier(0.5, 0, 0, 1)' origin="right" reset={true} >
         <Chip
           key={index} 
           className="bg-[#0AA5C1] text-white cursor-pointer border-none"
@@ -17,7 +19,9 @@ export default function Chips() {
         >
           {chip}
         </Chip>
+      </RevealWrapper >
       ))}
     </div>
+
   );
 }
